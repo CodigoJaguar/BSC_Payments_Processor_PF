c3cb9a9c3681dd46696380858aa7d6fa4ac8e802
CodigoJaguar <israel.san_mendoza@hotmail.com>
2023-03-03 09:03:05 -0600

    Get Deno in 1

--- build/index.js
+++ build/index.js
@@ -1,17 +1,25 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("source-map-support/register");
-// 3p
-const core_1 = require("@foal/core");
-// App
-const app_controller_1 = require("./app/app.controller");
-const db_1 = require("./db");
-async function main() {
-    await db_1.dataSource.initialize();
-    const app = await (0, core_1.createApp)(app_controller_1.AppController);
-    const port = core_1.Config.get('port', 'number', 3001);
-    app.listen(port, () => (0, core_1.displayServerURL)(port));
-}
-main()
-    .catch(err => { console.error(err.stack); process.exit(1); });
-//# sourceMappingURL=index.js.map
+// "use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+// require("source-map-support/register");
+// // 3p
+// const core_1 = require("@foal/core");
+// // App
+// const app_controller_1 = require("./app/app.controller");
+// const db_1 = require("./db");
+// async function main() {
+//     await db_1.dataSource.initialize();
+//     const app = await (0, core_1.createApp)(app_controller_1.AppController);
+//     const port = core_1.Config.get('port', 'number', 3001);
+//     app.listen(port, () => (0, core_1.displayServerURL)(port));
+// }
+// main()
+//     .catch(err => { console.error(err.stack); process.exit(1); });
+// //# sourceMappingURL=index.js.map
+
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve((_req) => {
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
